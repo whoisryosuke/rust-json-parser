d1956f1a9b844f7a6e297c72a3989b4f10000d3d
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-11-02 14:56:52 -0700

    :sparkles: t heme struct + example

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,15 @@ fn main() {
     println!("Hello, world!");
 
     // Grab JSON file
-    let file_path = "data/test.json".to_owned();
+    let file_path = "data/theme.json".to_owned();
     let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
 
-    parser::typed_example(&contents);
+    // Parse the JSON
+    let result = parser::typed_example(&contents);
+
+    // Handle errors from the parser if any
+    match result {
+        Ok(result) => (),
+        Err(error) => print!("{}", error),
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,21 +1,38 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
 #[derive(Serialize, Deserialize)]
-struct Person {
-    name: String,
-    age: u8,
-    phones: Vec<String>,
+struct Theme {
+    colors: HashMap<String, String>,
+    space: Vec<i32>,
+    font_sizes: Vec<i32>,
+    fonts: HashMap<String, String>,
+    font_weights: HashMap<String, i32>,
+    line_heights: HashMap<String, f32>,
+    breakpoints: HashMap<String, String>,
+    animation: HashMap<String, String>,
+    gradients: HashMap<String, String>,
 }
 
 pub fn typed_example(json_data: &str) -> Result<()> {
     // Parse the string of data into a Person object. This is exactly the
     // same function as the one that produced serde_json::Value above, but
     // now we are asking it for a Person as output.
-    let p: Person = serde_json::from_str(json_data)?;
+    let p: Theme = serde_json::from_str(json_data)?;
 
     // Do things just like with any other Rust data structure.
-    println!("Please call {} at the number {}", p.name, p.phones[0]);
+    println!(
+        "Black Color: {} \n Smallest Font Size: {}",
+        p.colors.get("black").unwrap(),
+        p.font_sizes[0]
+    );
+
+    // Loop over all the colors
+    for (key, color) in p.colors {
+        println!("{}: {}", key, color);
+    }
 
     Ok(())
 }
