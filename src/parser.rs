1ad4933c285106617153e1f6ea3d4683aed5d801
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-11-03 00:02:03 -0700

    :zap: convert to css var + gen css + keep tokens

--- src/parser.rs
+++ src/parser.rs
@@ -16,6 +16,19 @@ struct Theme {
     gradients: HashMap<String, String>,
 }
 
+#[derive(Default)]
+struct ThemeTokens {
+    colors: HashMap<String, String>,
+    space: HashMap<String, String>,
+    font_sizes: HashMap<String, String>,
+    fonts: HashMap<String, String>,
+    font_weights: HashMap<String, String>,
+    line_heights: HashMap<String, String>,
+    breakpoints: HashMap<String, String>,
+    animation: HashMap<String, String>,
+    gradients: HashMap<String, String>,
+}
+
 pub fn typed_example(json_data: &str) -> Result<()> {
     // Parse the string of data into a Person object. This is exactly the
     // same function as the one that produced serde_json::Value above, but
@@ -29,10 +42,38 @@ pub fn typed_example(json_data: &str) -> Result<()> {
         p.font_sizes[0]
     );
 
+    // We store the CSS variables we create to pass back to user for theming across app
+    // Initialize the theme tokens struct
+    let mut theme_tokens: ThemeTokens = ThemeTokens::default();
+
+    // Create a stylesheet to pass lines of CSS into
+    // Custom properties go into `:root`, media queries are separate
+    // Combined for simplicity now
+    // but starts to require a struct here (or leaning on a CSS crate/parser)
+    let mut stylesheet = Vec::new();
+
     // Loop over all the colors
     for (key, color) in p.colors {
-        println!("{}: {}", key, color);
+        // Create the custom property
+        let custom_property = format!("--colors-{}", key);
+        let css_rule = format!("{}: {};", &custom_property, color);
+
+        // @TODO: Export a CSS stylesheet file (or return CSS)
+        println!("{}", css_rule);
+        stylesheet.push(css_rule);
+
+        // Add the custom property
+        theme_tokens.colors.insert(key, custom_property);
     }
+
+    println!(
+        "Token colors.text is var({});",
+        theme_tokens.colors.get("text").unwrap()
+    );
+
+    println!("Stylesheet");
+
+    println!("{}", stylesheet.join("\n"));
 
     Ok(())
 }
